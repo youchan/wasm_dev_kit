c61dbcc594256c43423ae5ec464d720ae7c3ed3b
youchan <youchan01@gmail.com>
2025-01-02 18:11:35 +0900

    bundle gem wasm_dev_kit

---
+++ lib/wasm_dev_kit.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "wasm_dev_kit/version"
+
+module WasmDevKit
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/wasm_dev_kit/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module WasmDevKit
+  VERSION = "0.1.0"
+end
